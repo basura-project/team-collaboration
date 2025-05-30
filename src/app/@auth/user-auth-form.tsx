8e8e3275616b2bd77f3b8b9611cb3d9ef57260d2
l4k5hm4n <a.sailakshman@gmail.com>
2025-05-30 17:57:54 +0530

    removed deafult login details

--- src/app/@auth/user-auth-form.tsx
+++ src/app/@auth/user-auth-form.tsx
@@ -47,8 +47,8 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "superadmin",
-      password: "supersecretpassword",
+      username: "",
+      password: "",
     },
   });
 
--- src/app/@employee/default.tsx
+++ src/app/@employee/default.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <>This is a Employee page</>;
+  return <></>;
 }
